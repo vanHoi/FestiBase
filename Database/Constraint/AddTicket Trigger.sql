ec809c86987f31cba96596bfb18953cc8e79e00e
Leon Chen <leonchen@live.nl>
2017-12-13 16:00:52 +0100

    AddTicket afgemaakt met tests en al

--- Database/Constraint/AddTicket Trigger.sql
+++ Database/Constraint/AddTicket Trigger.sql
@@ -1,7 +1,12 @@
-﻿DROP TRIGGER if exists trg_valid_ticket
+﻿DROP PROCEDURE if exists sp_valid_ticket
 GO
-CREATE TRIGGER trg_valid_ticket ON TICKET_TYPE
-AFTER INSERT
+CREATE PROCEDURE sp_valid_ticket
+@festival_number INT,
+@branch_number INT,
+@ticket_type VARCHAR(50),
+@price money,
+@date_valid_from DATETIME,
+@date_valid_to DATETIME
 AS
 BEGIN
 	IF @@ROWCOUNT = 0
@@ -10,15 +15,17 @@ BEGIN
 	BEGIN TRY
 		IF(
 			NOT EXISTS(
-				SELECT * FROM Festival f
-				INNER JOIN Inserted i ON i.festival_number = f.festival_number
-				WHERE i.date_valid_from BETWEEN f.start_date AND f.end_date AND
-				i.date_valid_to BETWEEN f.start_date AND f.end_date
+				SELECT * FROM Festival
+				WHERE festival_number = @festival_number AND
+				@date_valid_from BETWEEN start_date AND end_date AND
+				@date_valid_to BETWEEN start_date AND end_date
 			)
 		)
 		BEGIN
 			;THROW 50001, 'Ticket datums liggen niet tussen de start- en einddatum van het festival.', 1
 		END
+
+		INSERT INTO TICKET_TYPE VALUES (@festival_number, @branch_number, @ticket_type, @price, @date_valid_from, @date_valid_to)
 	END TRY
 	BEGIN CATCH
 		;THROW
@@ -41,6 +48,15 @@ INSERT INTO Town VALUES (1, 'Nijmegen')
 
 INSERT INTO COMPANY_BRANCH VALUES ('01010101', 1, 'Heyendaalseweg', 1)
 
-INSERT INTO FESTIVAL_COMPANY VALUES ('01010101', 3, 'L. Chen', 'Verkoopt tickets', '0612345678')
+INSERT INTO FESTIVAL_COMPANY VALUES (1, 1, 'L. Chen', 'Verkoopt tickets', '0612345678')
 
-INSERT INTO TICKET_TYPE VALUES ('01010101', 3, 'Normale ticket', 500.00, '13-12-2017 11:00:00', '17-12-2017 23:00:00')
+--Startdatum te vroeg
+EXEC sp_valid_ticket 1, 1, 'VIP ticket', 500.00, '13-12-2017 11:00:00', '17-12-2017 22:00:00'
+
+--Einddatum is te laat
+EXEC sp_valid_ticket 1, 1, 'VIP ticket', 500.00, '13-12-2017 13:00:00', '17-12-2017 23:30:00'
+
+--Goed
+EXEC sp_valid_ticket 1, 1, 'VIP ticket', 500.00, '13-12-2017 13:00:00', '17-12-2017 22:00:00'
+
+--INSERT INTO TICKET_TYPE VALUES (1, 1, 'Normale ticket', 500.00, '13-12-2017 13:00:00', '17-12-2017 22:30:00')
