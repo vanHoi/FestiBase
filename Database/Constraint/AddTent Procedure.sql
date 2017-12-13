7d5193f8345aa67cc08eab9ec7ada81695d3c44d
Ivo Reumkens <ivoreum@gmail.com>
2017-12-13 10:13:09 +0100

    Combined update and insert procedure

--- Database/Constraint/AddTent Procedure.sql
+++ Database/Constraint/AddTent Procedure.sql
@@ -1,7 +1,9 @@
-DROP PROC IF EXISTS procAddTent;
+DROP PROC IF EXISTS procAddOrUpdateTent;
 GO
-CREATE PROC procAddTent
-	@podium_number INT,
+CREATE PROC procAddOrUpdateTent
+	@insert BIT,
+	@tent_number INT = NULL,
+	@podium_number INT = NULL,
 	@width INT,
 	@length INT,
 	@side_height INT,
@@ -45,8 +47,21 @@ BEGIN
 
 		END
 
-  INSERT INTO TENT VALUES (@podium_number, @length, @width, @side_height, @ridge_height,
+	IF (@insert = 1)
+	BEGIN
+		INSERT INTO TENT VALUES (@podium_number, @length, @width, @side_height, @ridge_height,
                            @construction_width, @construction_length, @tent_type, @color, @floor_type, @capacity)
+	END
+	ELSE 
+	BEGIN
+		IF (@tent_number = null)
+		BEGIN
+			;THROW 50005, 'You must supply a tent number with an update statement.', 1
+		END
+		UPDATE TENT SET podium_number = @podium_number, length =  @length, width = @width, side_height = @side_height, ridge_height = @ridge_height,
+                           construction_width = @construction_width, construction_length = @construction_length, tent_type = @tent_type, color = @color, 
+						   floor_type = @floor_type, capacity = @capacity WHERE tent_number = @tent_number 
+	END
 
   END TRY
 
@@ -65,20 +80,31 @@ INSERT INTO Podium (festival_number, name, podium_width, podium_depth, height, c
 DECLARE @podium_number INT;
 SELECT @podium_number = podium_number FROM PODIUM WHERE festival_number = 1 AND name = 'Test Stage'
 
-/* Correct */
-EXEC procAddTent @podium_number, 1200, 2000, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+/* Correcte insert */
+EXEC procAddOrUpdateTent 1, null, @podium_number, 1200, 2000, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+-- Search for the tent number
+DECLARE @tent_number INT;
+SELECT @tent_number = MAX(tent_number) FROM TENT
 
 /* Tent not wide enough for the podium */
-EXEC procAddTent @podium_number, 800, 2000, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+EXEC procAddOrUpdateTent 1, null, @podium_number, 800, 2000, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+EXEC procAddOrUpdateTent 0, @tent_number, @podium_number, 800, 2000, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
 
 /* Tent not deep enough for the podium */
-EXEC procAddTent @podium_number, 1200, 500, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+EXEC procAddOrUpdateTent 1, null, @podium_number, 1200, 500, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+EXEC procAddOrUpdateTent 0, @tent_number, @podium_number, 1200, 500, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
 
 /* Tent side height are not high enough for the podium */
-EXEC procAddTent @podium_number, 3000, 2000, 200, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+EXEC procAddOrUpdateTent 1, null, @podium_number, 1200, 2000, 200, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+EXEC procAddOrUpdateTent 0, @tent_number, @podium_number, 1200, 2000, 200, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
 
 /* Tent to small for the podium */
-EXEC procAddTent @podium_number, 3000, 2000, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 8000
+EXEC procAddOrUpdateTent 1, null, @podium_number, 1200, 2000, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 8000
+EXEC procAddOrUpdateTent 0, @tent_number, @podium_number, 1200, 2000, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 8000
 
 /* Not enough standing room (less then 3 meters) on the podium */
-EXEC procAddTent @podium_number, 3000, 2000, 400, 400, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+EXEC procAddOrUpdateTent 1, null, @podium_number, 1200, 2000, 400, 400, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+EXEC procAddOrUpdateTent 0, @tent_number, @podium_number, 1200, 2000, 400, 400, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
+
+/* Correcte update */
+EXEC procAddOrUpdateTent 1, @tent_number, @podium_number, 3000, 2000, 500, 1000, 1400, 3300, 'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000
