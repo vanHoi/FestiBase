b94bfe595870fdf91a67f62759e3921a8d9b2778
Leon Chen <leonchen@live.nl>
2017-12-15 12:29:36 +0100

    fatsoenlijke project aangemaakt zodat er nu eindelijk getest kan worden, database connectie werkt (lokaal)

---
+++ Website/Website/App_Code/DatabaseTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+public class DatabaseTest
+{
+    private SqlConnection conn;
+
+    public DatabaseTest()
+    {
+        conn = new SqlConnection("user id=sa;" +
+                                "password=wachtwoord123;" +
+                                "server=localhost;" +
+                                "Trusted_Connection=yes;" +
+                                "database=FestiBase; " +
+                                "connection timeout=30");
+
+        try
+        {
+            conn.Open();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public string getFestivalbyNumber(int number)
+    {
+        try
+        {
+            string festival = "";
+            SqlCommand command = new SqlCommand("SELECT * FROM FESTIVAL WHERE festival_number = " + number, conn);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                festival = String.Format("Festival: {0} start op {1} en eindigt op {2}. Locatie {3}", reader["name"].ToString(),
+                    reader["start_date"].ToString(), reader["end_date"].ToString(), reader["location"].ToString());
+            }
+
+            conn.Close();
+            return festival;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        return null;
+    }
+}
---
+++ Website/Website/Test.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Test : System.Web.UI.Page
+{
+    DatabaseTest databaseTest;
+    string festivalDetails;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        databaseTest = new DatabaseTest();
+        festivalDetails = "";
+    }
+
+    public string getFestivalFromDatabase(int number)
+    {
+        return databaseTest.getFestivalbyNumber(number);
+    }
+
+    public string getFestivalDetails()
+    {
+        return festivalDetails;
+    }
+
+    protected void btnFestival_Click(object sender, EventArgs e)
+    {
+        if (!String.IsNullOrWhiteSpace(tbFestivalNumber.Text))
+        {
+            festivalDetails = getFestivalFromDatabase(Convert.ToInt32(tbFestivalNumber.Text));
+        }
+    }
+}
