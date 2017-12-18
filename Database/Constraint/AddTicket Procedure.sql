c68913c436923a7ef59b93a76958ce10950331ac
Leon Chen <leonchen@live.nl>
2017-12-18 10:41:10 +0100

    beschrijving en transacties toegevoegd aan constraint.

--- Database/Constraint/AddTicket Procedure.sql
+++ Database/Constraint/AddTicket Procedure.sql
@@ -1,4 +1,17 @@
-﻿DROP PROCEDURE if exists sp_add_ticket_type
+﻿/*==============================================================*/
+/* DBMS name:		FestiBase									*/
+/* PDM version 5												*/	
+/* Constraint													*/
+/*==============================================================*/
+
+/*
+Leon Chen
+Procedure to add a new ticket_type to the database
+Constraint:	6
+*/
+
+
+DROP PROCEDURE if exists sp_add_ticket_type
 GO
 CREATE PROCEDURE sp_add_ticket_type
 @festival_number INT,
@@ -38,13 +51,21 @@ GO
 SET DATEFORMAT ymd
 
 --date_valid_from is too early
+BEGIN TRAN
 EXEC sp_add_ticket_type 1, 10, 'VIP ticket', 500.00, '2017-07-14 23:00:00', '2017-07-17 00:00:00'
+ROLLBACK TRAN
 
 --date_valid_to is too late
+BEGIN TRAN
 EXEC sp_add_ticket_type 1, 10, 'VIP ticket', 500.00, '2017-07-15 00:00:00', '2017-07-17 01:00:00'
+ROLLBACK TRAN
 
 --Works, Date is exactly on the start and end-date of the festival
+BEGIN TRAN
 EXEC sp_add_ticket_type 1, 10, 'VIP ticket', 500.00, '2017-07-15 00:00:00', '2017-07-17 00:00:00'
+ROLLBACK TRAN
 
 --Works
+BEGIN TRAN
 EXEC sp_add_ticket_type 1, 10, 'Normal ticket', 500.00, '2017-07-15 05:00:00', '2017-07-16 23:00:00'
+ROLLBACK TRAN
