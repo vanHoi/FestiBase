a75a0c7ccee018a3cf368c797ea635be3c31ddd4
Leon Chen <leonchen@live.nl>
2017-12-19 12:17:31 +0100

    tests gefixt, werkt ook nu met de nieuwste ddl script

--- Database/Constraint/AddTicket Procedure.sql
+++ Database/Constraint/AddTicket Procedure.sql
@@ -1,6 +1,6 @@
 ﻿/*==============================================================*/
 /* DBMS name:		FestiBase									*/
-/* PDM version:		5											*/
+/* PDM version:		6											*/
 /* Last edited:		18/12/2017									*/
 /* Author:			Leon Chen									*/
 /* Constraint													*/
@@ -16,8 +16,7 @@ Constraint:	6
 DROP PROCEDURE if exists sp_add_ticket_type
 GO
 CREATE PROCEDURE sp_add_ticket_type
-@festival_number INT,
-@branch_number INT,
+@festival_company_number INT,
 @ticket_type VARCHAR(50),
 @price money,
 @date_valid_from DATETIME,
@@ -30,8 +29,9 @@ BEGIN
 	BEGIN TRY
 		IF(
 			NOT EXISTS(
-				SELECT * FROM Festival
-				WHERE festival_number = @festival_number AND
+				SELECT * FROM Festival f
+				INNER JOIN FESTIVAL_COMPANY fc ON f.festival_number = fc.festival_number
+				WHERE fc.festival_company_number = @festival_company_number AND
 				@date_valid_from BETWEEN start_date AND end_date AND
 				@date_valid_to BETWEEN start_date AND end_date
 			)
@@ -40,7 +40,7 @@ BEGIN
 			;THROW 50001, 'The valid dates of the ticket are not between the start and enddate of the festival.', 1
 		END
 
-		INSERT INTO TICKET_TYPE VALUES (@festival_number, @branch_number, @ticket_type, @price, @date_valid_from, @date_valid_to)
+		INSERT INTO TICKET_TYPE VALUES (@festival_company_number, @ticket_type, @price, @date_valid_from, @date_valid_to)
 	END TRY
 	BEGIN CATCH
 		;THROW
@@ -49,25 +49,26 @@ END
 GO
 
 --Tests
---Execute this statement first
-SET DATEFORMAT ymd
+--Execute this first
+SET DATEFORMAT DMY
+GO
 
 --date_valid_from is too early
 BEGIN TRAN
-EXEC sp_add_ticket_type 1, 10, 'VIP ticket', 500.00, '2017-07-14 23:00:00', '2017-07-17 00:00:00'
+EXEC sp_add_ticket_type 10, 'Super ticket', 500.00, '13-04-2017 23:30:00', '16-04-2017 23:50:00'
 ROLLBACK TRAN
 
 --date_valid_to is too late
 BEGIN TRAN
-EXEC sp_add_ticket_type 1, 10, 'VIP ticket', 500.00, '2017-07-15 00:00:00', '2017-07-17 01:00:00'
+EXEC sp_add_ticket_type 10, 'Super ticket', 500.00, '14-04-2017 02:00:00', '16-04-2017 23:59:30'
 ROLLBACK TRAN
 
 --Works, Date is exactly on the start and end-date of the festival
 BEGIN TRAN
-EXEC sp_add_ticket_type 1, 10, 'VIP ticket', 500.00, '2017-07-15 00:00:00', '2017-07-17 00:00:00'
+EXEC sp_add_ticket_type 10, 'Super ticket', 500.00, '14-04-2017 00:00:00', '16-04-2017 23:59:00'
 ROLLBACK TRAN
 
 --Works
 BEGIN TRAN
-EXEC sp_add_ticket_type 1, 10, 'Normal ticket', 500.00, '2017-07-15 05:00:00', '2017-07-16 23:00:00'
+EXEC sp_add_ticket_type 10, 'Superkoele ticket', 500.00, '14-04-2017 08:00:00', '16-04-2017 23:00:00'
 ROLLBACK TRAN
