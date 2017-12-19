77e3cd0ad1e7b7fc0c90b62d5fdc76bc43017e7f
Leon Chen <leonchen@live.nl>
2017-12-19 14:27:41 +0100

    met update nu, constraints zijn ook los

--- Database/Constraint/AddTicket Procedure.sql
+++ Database/Constraint/AddTicket Procedure.sql
@@ -1,7 +1,7 @@
 ﻿/*==============================================================*/
 /* DBMS name:		FestiBase									*/
 /* PDM version:		6											*/
-/* Last edited:		18/12/2017									*/
+/* Last edited:		19/12/2017									*/
 /* Author:			Leon Chen									*/
 /* Constraint													*/
 /*==============================================================*/
@@ -20,27 +20,63 @@ CREATE PROCEDURE sp_add_ticket_type
 @ticket_type VARCHAR(50),
 @price money,
 @date_valid_from DATETIME,
-@date_valid_to DATETIME
+@date_valid_to DATETIME,
+@insert bit
 AS
 BEGIN
 	IF @@ROWCOUNT = 0
 		RETURN
 	SET NOCOUNT ON
 	BEGIN TRY
+		EXEC sp_check_ticket_type_start_end_date @festival_company_number, @date_valid_from, @date_valid_to
+
+		IF(@insert = 1)
+		BEGIN
+			INSERT INTO TICKET_TYPE VALUES (@festival_company_number, @ticket_type, @price, @date_valid_from, @date_valid_to)
+		END
+		ELSE BEGIN
+			UPDATE TICKET_TYPE SET festival_company_number = @festival_company_number, ticket_type = @ticket_type, price = @price,
+			date_valid_from = @date_valid_from, date_valid_to = @date_valid_to WHERE festival_company_number = @festival_company_number AND ticket_type = @ticket_type
+		END
+	END TRY
+	BEGIN CATCH
+		;THROW
+	END CATCH
+END
+GO
+
+DROP PROCEDURE if exists sp_check_ticket_type_start_end_date
+GO
+CREATE PROCEDURE sp_check_ticket_type_start_end_date
+@festival_company_number INT,
+@date_valid_from DATETIME,
+@date_valid_to DATETIME
+AS
+BEGIN
+	BEGIN TRY
 		IF(
 			NOT EXISTS(
 				SELECT * FROM Festival f
 				INNER JOIN FESTIVAL_COMPANY fc ON f.festival_number = fc.festival_number
 				WHERE fc.festival_company_number = @festival_company_number AND
-				@date_valid_from BETWEEN start_date AND end_date AND
+				@date_valid_from BETWEEN start_date AND end_date 
+			)
+		)
+		BEGIN
+			;THROW 50001, 'The start date of the ticket is not between the start and end date of the festival.', 1
+		END
+
+		IF(
+			NOT EXISTS(
+				SELECT * FROM Festival f
+				INNER JOIN FESTIVAL_COMPANY fc ON f.festival_number = fc.festival_number
+				WHERE fc.festival_company_number = @festival_company_number AND
 				@date_valid_to BETWEEN start_date AND end_date
 			)
 		)
 		BEGIN
-			;THROW 50001, 'The valid dates of the ticket are not between the start and enddate of the festival.', 1
+			;THROW 50002, 'The end date of the ticket is not between the start and end date of the festival.', 1
 		END
-
-		INSERT INTO TICKET_TYPE VALUES (@festival_company_number, @ticket_type, @price, @date_valid_from, @date_valid_to)
 	END TRY
 	BEGIN CATCH
 		;THROW
@@ -55,20 +91,26 @@ GO
 
 --date_valid_from is too early
 BEGIN TRAN
-EXEC sp_add_ticket_type 10, 'Super ticket', 500.00, '13-04-2017 23:30:00', '16-04-2017 23:50:00'
+EXEC sp_add_ticket_type 10, 'Super ticket', 500.00, '13-04-2017 23:30:00', '16-04-2017 23:50:00', 1
 ROLLBACK TRAN
 
 --date_valid_to is too late
 BEGIN TRAN
-EXEC sp_add_ticket_type 10, 'Super ticket', 500.00, '14-04-2017 02:00:00', '16-04-2017 23:59:30'
+EXEC sp_add_ticket_type 10, 'Super ticket', 500.00, '14-04-2017 02:00:00', '16-04-2017 23:59:30', 1
 ROLLBACK TRAN
 
 --Works, Date is exactly on the start and end-date of the festival
 BEGIN TRAN
-EXEC sp_add_ticket_type 10, 'Super ticket', 500.00, '14-04-2017 00:00:00', '16-04-2017 23:59:00'
+EXEC sp_add_ticket_type 10, 'Super ticket', 500.00, '14-04-2017 00:00:00', '16-04-2017 23:59:00', 1
 ROLLBACK TRAN
 
 --Works
 BEGIN TRAN
-EXEC sp_add_ticket_type 10, 'Superkoele ticket', 500.00, '14-04-2017 08:00:00', '16-04-2017 23:00:00'
+EXEC sp_add_ticket_type 10, 'Superkoele ticket', 500.00, '14-04-2017 08:00:00', '16-04-2017 23:00:00', 1
+ROLLBACK TRAN
+
+--Works with updates too
+BEGIN TRAN
+EXEC sp_add_ticket_type 10, 'Superkoele ticket', 500.00, '14-04-2017 08:00:00', '16-04-2017 23:00:00', 1
+EXEC sp_add_ticket_type 10, 'Superkoele ticket', 500.00, '14-04-2017 09:00:00', '16-04-2017 22:00:00', 0
 ROLLBACK TRAN
