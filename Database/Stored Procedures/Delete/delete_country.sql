2677c08604d9bd3eacc0f57d6c8400492a11dee0
iYuriZ <yuriz@live.nl>
2017-12-22 12:11:39 +0100

    aanpassing country

--- Database/Stored Procedures/Delete/delete_country.sql
+++ Database/Stored Procedures/Delete/delete_country.sql
@@ -21,7 +21,7 @@ BEGIN
 					   FROM COUNTRY
 				       WHERE country_number = @country_number)
 			BEGIN
-				;THROW 50000, 'This record does not exist', 1
+				;THROW 50000, 'This country does not exist', 1
 			END
 
 		DELETE FROM COUNTRY WHERE country_number = @country_number
