bf3352371b8befc927fdbd7d4bee84c0e7c8b4ae
verkerk20 <robertverkerk@hetnet.nl>
2017-12-22 10:48:43 +0100

    added procedure genre_preference_visitor

---
+++ Database/Stored Procedures/Delete/delete_genre_preference_visitor.sql
@@ -0,0 +1,46 @@
+﻿/*==============================================================*/
+/* DBMS name:		FestiBase									*/
+/* PDM version:		6											*/
+/* Last edited:		22-12-2017									*/
+/* Edited by:		Robert Verkerk								*/
+/* Procedure:		Delete genre	 						*/
+/*==============================================================*/
+USE FestiBase
+GO
+
+DROP PROCEDURE IF EXISTS sp_delete_genre_preference_visitor
+GO
+CREATE PROCEDURE sp_delete_genre_preference_visitor
+	@visitor_number INT,
+	@genre_number INT
+AS
+BEGIN
+	BEGIN TRY
+		IF NOT EXISTS (SELECT *
+				   FROM GENRE_preference_VISITOR
+				   WHERE genre_number = @genre_number
+				   AND visitor_number = @visitor_number)
+			BEGIN
+				;THROW 50000, 'This genre preference does not exist', 1
+			END
+
+		DELETE FROM GENRE_preference_VISITOR WHERE genre_number = @genre_number AND visitor_number = @visitor_number
+	END TRY
+	BEGIN CATCH
+		;THROW
+	END CATCH
+END
+GO
+
+/* tests */
+/* delete success */
+BEGIN TRAN
+EXEC sp_delete_genre_preference_visitor 1, 1
+ROLLBACK TRAN
+GO
+
+/* delete */
+BEGIN TRAN
+EXEC sp_delete_genre_preference_visitor 55, 1
+ROLLBACK TRAN
+GO
