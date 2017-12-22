8f92755da873419de04e4403deba55bbe3549b1d
iYuriZ <yuriz@live.nl>
2017-12-22 10:00:07 +0100

    delete_town

---
+++ Database/Stored Procedures/Delete/delete_town.sql
@@ -0,0 +1,40 @@
+﻿/*==============================================================*/
+/* DBMS name:		FestiBase									*/
+/* PDM version:		6											*/
+/* Last edited:		22-12-2017									*/
+/* Edited by:		Yuri Vannisselroy							*/
+/* Procedure:		Delete TOWN									*/
+/*==============================================================*/
+
+USE FestiBase
+GO
+
+DROP PROCEDURE IF EXISTS sp_delete_town
+GO
+CREATE PROCEDURE sp_delete_town
+	@town_number INT
+AS
+BEGIN
+	BEGIN TRY
+
+		IF NOT EXISTS (SELECT *
+					   FROM TOWN
+					   WHERE town_number = @town_number)
+			BEGIN
+				;THROW 50000, 'This record does not exist', 1
+			END
+
+		DELETE FROM TOWN WHERE town_number = @town_number
+
+	END TRY
+	BEGIN CATCH
+		;THROW
+	END CATCH
+END
+GO
+
+/* TEST (will trigger foreign key constraint) */
+BEGIN TRAN
+EXEC sp_delete_town 1
+ROLLBACK TRAN
+GO
