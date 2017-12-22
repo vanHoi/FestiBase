0b912eafd1c08113bc6e396880c5f2df8f5e6ad7
iYuriZ <yuriz@live.nl>
2017-12-22 12:22:26 +0100

    aanpassing visitor

--- Database/Stored Procedures/Delete/delete_visitor.sql
+++ Database/Stored Procedures/Delete/delete_visitor.sql
@@ -21,7 +21,7 @@ BEGIN
 					   FROM VISITOR
 				       WHERE visitor_number = @visitor_number)
 			BEGIN
-				;THROW 50000, 'This record does not exist', 1
+				;THROW 50000, 'This visitor does not exist', 1
 			END
 
 		DELETE FROM VISITOR WHERE visitor_number = @visitor_number
