650671d16578e61ae90480086e4713f6e264edc3
iYuriZ <yuriz@live.nl>
2017-12-22 12:08:03 +0100

    aanpassing tow

--- Database/Stored Procedures/Delete/delete_town.sql
+++ Database/Stored Procedures/Delete/delete_town.sql
@@ -21,7 +21,7 @@ BEGIN
 					   FROM TOWN
 					   WHERE town_number = @town_number)
 			BEGIN
-				;THROW 50000, 'This record does not exist', 1
+				;THROW 50000, 'This town does not exist', 1
 			END
 
 		DELETE FROM TOWN WHERE town_number = @town_number
