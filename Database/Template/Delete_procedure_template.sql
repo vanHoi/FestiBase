2696637231343aa653d42fcbb5db6fdf63c366fe
iYuriZ <yuriz@live.nl>
2017-12-22 09:45:04 +0100

    nieuwe templates

---
+++ Database/Template/Delete_procedure_template.sql
@@ -0,0 +1,21 @@
+﻿DROP PROCEDURE IF EXISTS sp_delete
+GO
+CREATE PROCEDURE sp_delete
+	@surrogate_key INT
+AS
+BEGIN
+	BEGIN TRY
+		IF NOT EXISTS (SELECT *
+				   FROM tabel
+				   WHERE kolom = @surrogate_key)
+			BEGIN
+				;THROW 50000, 'This record does not exist', 1
+			END
+
+		DELETE FROM tabel WHERE kolom = @surrogate_key
+	END TRY
+	BEGIN CATCH
+		;THROW
+	END CATCH
+END
+GO
---
+++ Database/Template/Trigger_template.sql
@@ -0,0 +1,27 @@
+﻿/*
+	Trigger naam begint met sp_
+	Procedure naam niet in camelcases, maar kleine letters en _
+	Gebruik een ;THROW, niet een RAISERROR.
+	@@ROWCOUNT voor efficiëntie
+	Voeg de bovenstaande doc toe
+*/
+DROP TRIGGER IF EXISTS trg_
+GO
+CREATE TRIGGER trg_ ON TABLE
+AFTER UPDATE
+AS
+BEGIN
+	IF @@ROWCOUNT = 0
+		RETURN
+	SET NOCOUNT ON
+    BEGIN TRY
+	-->
+		BEGIN
+			;THROW 50001, '', 1
+		END
+    END TRY
+    BEGIN CATCH
+        ;THROW
+    END CATCH
+END
+GO
