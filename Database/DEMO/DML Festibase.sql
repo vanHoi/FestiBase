111654fcc0ac3413f9b301f8ed84cb1cac651bea
iYuriZ <yuriz@live.nl>
2018-01-10 10:08:11 +0100

    DEMO voorbereiden

---
+++ Database/DEMO/DML Festibase.sql
@@ -0,0 +1,256 @@
+/*==============================================================*/
+/* DBMS name:      FestiBase									*/
+/* PDM version 6												*/
+/* Last edited: 19-12-2017										*/
+/* Testdata													    */
+/* Yuri Vannisselroij, Robert Verkerk, Ivo Reumkens				*/
+/*==============================================================*/
+
+USE FestiBase
+GO
+
+SET DATEFORMAT DMY
+GO
+
+INSERT INTO ORGANISATION (name) VALUES
+('Paaspop'),
+('Pinkpop'),
+('Emporium'),
+('Rock Am')
+GO
+
+INSERT INTO FESTIVAL (organisation_number, name, start_date, end_date, location, token_price) VALUES
+(1, 'Paaspop 2017', convert(datetime, '14-04-2017 00:00:00', 105), convert(datetime, '16-04-2017 23:59:00', 105), 'Schijndel', '2.50'),
+(1, 'Paaspop 2018', convert(datetime, '30-03-2018 00:00:00', 105), convert(datetime, '01-04-2018 23:59:00', 105), 'Schijndel', '3.00'),
+(2, 'Pinkpop 2018', convert(datetime, '15-06-2018 00:00:00', 105), convert(datetime, '17-06-2018 00:00:00', 105), 'Megaland Landgraaf', '3.00'),
+(4, 'Rock Am Ring 2018', convert(datetime, '1-06-2018 00:00:00', 105), convert(datetime, '03-06-2018 23:59:00', 105), 'Megaland Landgraaf', '3.00')
+GO
+
+INSERT INTO COUNTRY (name) VALUES
+('Nederland'),
+('Duitsland'),
+('Belgie')
+GO
+
+INSERT INTO TOWN (country_number, name) VALUES
+(1, 'Cuijk'),
+(1, 'Nijmegen'),
+(1, 'Schijndel'),
+(1, 'Landgraaf'),
+(2, 'Nürburg')
+GO
+
+INSERT INTO COMPANY (coc_number, name) VALUES
+(23456001,'Henks Hamburgers'),
+(23456002,'Roys Reuzenrad'),
+(23456003,'Verkerk Verhuur'),
+(23456004,'Piets Podiums'),
+(23456005,'Heineken'),
+(23456006,'Aries Attracties'),
+(23456007,'Tentencompany'),
+(23456008,'Stagecompany'),
+(23456045, 'Burger King'),
+(23456046, 'Roberts Inbreekservices'),
+(23456048, 'Leons Kaartjes Oplichterijen'),
+(23456060, 'D-Records'),
+(23456061, 'Music HQ')
+GO
+
+INSERT INTO COMPANY_BRANCH (coc_number, town_number, street, house_number) VALUES
+(23456003, 1, 'aleidestraat', '15'),
+(23456005, 2, 'Hatertseweg', '123'),
+(23456006, 1, 'schijndelseweg', '5'),
+(23456006, 2, 'maaskantjeseweg', '5'),
+(23456001, 3, 'grotestraat', '2'),
+(23456001, 2, 'hondenkaas', '25'),
+(23456005, 1, 'datdorp', '27'),
+(23456045, 1, 'Coolestraat', '28'),
+(23456045, 3, 'Coolestraat', '28'),
+(23456048, 3, 'Oplichtersstraat', '1'),
+(23456060, 2, 'Lankforst', '29'),
+(23456061, 2, 'Lankforst', '28')
+GO
+
+INSERT INTO FESTIVAL_COMPANY (branch_number, festival_number, contact_person, description, telephone_number) VALUES
+(1, 1, 'Robert Verkerk', 'Tent verhuur', '0612345678'),
+(2, 1, 'Freddie Heineken', 'Ja, bier!!', '0271234567'),
+(3, 1, 'Henk van Amstel', 'Attractieverhuur', '0698765432'),
+(4, 2, 'Klaas van Heineken', 'Attractieverhuur', '0698478512'),
+(5, 3, 'Frida Heemskerk', 'hamburgerbar', '0611122233'),
+(6, 2, 'Mac Donalds', 'Voedselcatering', '0624577885'),
+(7, 3, 'Ham Burgers', 'Hamburgerbar', '0687433772'),
+(8, 1, 'Robert De Inbreker', 'Kluisjesverhuurdbedrijf', '0654188745'),
+(9, 2, 'Robert De Inbreker', 'Kluisjesverhuurdbedrijf', '0654188745'),
+(10, 1, 'Leon De Oplichter', 'Ticket bedrijf', '0654788412'),
+(11, 1, 'Dirk Records',  'Manager 3ROBI', '0689702314'),
+(12, 1, 'Mary Headquarters', 'Manager B-Front', '0612449983')
+GO
+
+INSERT INTO ARTIST (name) VALUES
+('3ROBI'),
+('B-Front'),
+('De Toppers'),
+('Justin Bieber'),
+('Big Shaq'),
+('Eminem'),
+('Rapper Sjors'),
+('Pearl Jam'),
+('Foo Fighters'),
+('Bruno Mars')
+GO
+
+INSERT INTO ARTIST_FILE (artist_number, festival_company_number, "file", description) VALUES
+(1, 11, 'contract 3ROBI', 'Het contract van 3ROBI'),
+(2, 12, 'contract B-Front', 'Het contract van B-Front'),
+(9, 12, 'Setlist Foo Fighters', 'De setlist van de Foo Fighters')
+
+INSERT INTO ATTRACTION (festival_company_number, name, attraction_type) VALUES
+(1, 'Henks Draaimolen', 'Draaimolen'),
+(2, 'Booster', 'Booster'),
+(3, 'Booster', 'Booster'),
+(3, 'Das Omen', 'Spookhuis')
+GO
+
+INSERT INTO VISITOR (town_number, email, first_name, surname, telephone_number, birthdate, twitter_username, facebook_username) VALUES
+(1, 'robertverkerk@hetnet.nl', 'Robert', 'Verkerk', '0612345678', convert(datetime, '15-07-1995 00:00:00', 105), 'robert_verkerk', NULL),
+(1, 'mariusssss@gmail.com', 'Mariuszsz', 'Blautzik', '0632165487', convert(datetime, '12-02-1993 00:00:00', 105), NULL, 'mariusBlautzik'),
+(2, 'ivorr@reumkens.nl', 'Ivo', 'Reumkens', '0632141587', convert(datetime, '12-02-1996 00:00:00', 105), NULL, NULL),
+(null, null, null, null, null, null, null, null)
+GO
+
+INSERT INTO TICKET_TYPE (festival_company_number, ticket_type, price, date_valid_from, date_valid_to) VALUES
+(10, 'Premium ticket', '75', convert(datetime, '30-03-2018 00:00:00', 105), convert(datetime, '01-04-2018 23:59:00', 105)),
+(10, 'VIP', '120', convert(datetime, '14-04-2014 00:00:00', 105), convert(datetime, '16-04-2014 23:59:00', 105)),
+(10, 'dagticket', '50', convert(datetime, '01-06-2017 00:00:00', 105), convert(datetime, '04-06-2017 02:00:00', 105))
+GO
+
+INSERT INTO BOUGHT_TICKET (festival_company_number, ticket_type, visitor_number, scan_date) VALUES
+(10, 'Premium ticket', 1, null),
+(10, 'Premium ticket', 1, null),
+(10, 'VIP', 2, null)
+GO
+
+INSERT INTO CATERING (festival_company_number, name, electricity) VALUES
+(1, 'Hamburgerbar', 1),
+(2, 'Friettent', 1),
+(7, 'Tacos', 1),
+(8, 'Bierkar', 0)
+GO
+
+INSERT INTO FESTIVAL_COMPANY_FILE (festival_company_number, "file", description) VALUES
+(1, 'path/to/file.pdf', 'factuur huur tent'),
+(3, 'file/path.jpg', 'afbeelding attractie')
+GO
+
+INSERT INTO GENRE (genre) VALUES
+('Rap'),
+('Nederlands'),
+('Pop'),
+('Hardstyle')
+GO
+
+INSERT INTO GENRE_of_ARTIST (genre_number, artist_number) VALUES
+(1, 1),
+(3, 1),
+(2, 3),
+(4, 7)
+GO
+
+INSERT INTO GENRE_preference_VISITOR (visitor_number, genre_number) VALUES
+(1, 1),
+(1, 2),
+(2, 3),
+(3, 4)
+GO
+
+INSERT INTO LOCKER (festival_company_number) VALUES
+(8),
+(8),
+(8),
+(8),
+(8),
+(8),
+(8),
+(8),
+(8),
+(8),
+(8),
+(8),
+(8),
+(8),
+(8),
+(8)
+GO
+
+INSERT INTO LOCKER_RENTED (locker_number, visitor_number, start_date, end_date) VALUES
+(1, 1, convert(datetime, '15-07-2017 00:00:00', 105), convert(datetime, '17-07-2017 00:00:00', 105)),
+(2, 3, convert(datetime, '15-07-2017 00:00:00', 105), null)
+GO
+
+INSERT INTO TENT (festival_number, name, width, length, side_height, ridge_height, construction_width, construction_length, tent_type, color, floor_type, capacity) VALUES
+(1, 'tent1', 5000, 3000, 240, 400, 5200, 3200, 'aluhal', 'wit', 'dansvloer', 10000),
+(2, 'tent2', 5000, 3000, 240, 400, 5200, 3200, 'pagode', 'wit', 'cassetevloer', 1000),
+(1, 'Hele Grote Tent', 1300, 2000, 500, 1000, 1500, 3300, 'Grote Tent voor meerdere podiums', 'Blauw-wit', 'Systeemvloer', 12000) 
+GO
+
+INSERT INTO PODIUM (festival_number, tent_number, name, construction_width, construction_length, construction_height, floor_height, capacity, floor_load, free_span_width, free_span_length, free_span_height, environment) VALUES
+(1, NULL, 'Heineken Stage', 1500, 1000, 700, 100, 12000, 100, 1400, 800, 600, 'description of environment'),
+(1, 2, 'PopPodium', 1500, 1200, 700, null, 12000, 50, 1300, 1000, 600, 'beautiful trees'),
+(3, NULL, 'Main Stage', 3500, 1200, 800, 150, 12000, 120, 800, 700, 600, 'Hele grote open ruimte')
+GO
+
+INSERT INTO PODIUM_SCHEDULE (podium_number, start_date, start_time, end_date, end_time, break_time) VALUES
+(1, '30-03-2018', '14:00:00', '30-03-2018', '23:00:00', 15),
+(2, '31-03-2018', '14:00:00', '31-03-2018', '23:00:00', 60),
+(3, '15-06-2018', '14:00:00', '15-06-2018', '22:30:00', 60),
+(3, '16-06-2018', '14:00:00', '15-06-2018', '23:00:00', 60),
+(3, '17-06-2018', '16:00:00', '17-06-2018', '02:00:00', 60)
+GO
+
+INSERT INTO PERFORMANCE (artist_number, podium_schedule_number, festival_number, start_time, play_time, min_prep_time) VALUES
+(1, 1, 2, convert(varchar(8), NULL, 108), 60,  15),
+(2, 1, 2, convert(varchar(8), NULL, 108), 30,  30),
+(3, 1, 2, convert(varchar(8), NULL, 108), 60,  15),
+(4, 1, 2, convert(varchar(8), NULL, 108), 75,  60),
+(5, 2, 2, convert(varchar(8), '10:00:00', 108), 75,  15),
+(6, 2, 2, convert(varchar(8), '18:00:00', 108), 120, 15),
+(7, 2, 2, convert(varchar(8), '20:15:00', 108), 60,  15),
+(1, 2, 2, convert(varchar(8), '22:15:00', 108), 90,  15),
+(8, 3, 2, convert(varchar(8), '21:15:00', 108), 90,  30),
+(9, 4, 2, convert(varchar(8), '21:15:00', 108), 90,  30),
+(10, 5, 3, convert(varchar(8), '21:15:00', 108), 90,  45)
+GO
+
+INSERT INTO PODIUM_FILE(podium_number, festival_company_number, "file", description) VALUES
+(1, 1, 'path/to/buildingbook.pdf', 'a beautifl description'),
+(2, 1, 'path/to/file.pdf', 'a description')
+GO
+
+INSERT INTO PODIUM_plays_GENRE (genre_number, podium_number) VALUES
+(1, 1),
+(2, 2)
+GO
+
+INSERT INTO TENT_FILE(tent_number, festival_company_number, "file", description) VALUES
+(1, 1, 'path/to/file.pdf', 'factuur huur tent'),
+(2, 1, 'file/path.jpg', 'afbeelding attractie')
+GO
+
+INSERT INTO TOILET (festival_company_number, name, capacity) VALUES
+(1, 'Toilet 1', 500),
+(1, 'Toilet 2', 50)
+GO
+
+INSERT INTO VISITOR_likes_PERFORMANCE (visitor_number, performance_number) VALUES
+(1, 2),
+(1, 1),
+(2, 1)
+GO
+
+INSERT INTO VISITOR_VISITED_PERFORMANCE (visitor_number, performance_number) VALUES
+(1,2),
+(1,1),
+(2,1)
+GO
+
+
---
+++ Database/DEMO/Demonstratie.sql
@@ -0,0 +1,265 @@
+﻿/* ================================	*/
+/*			Demonstratie			*/
+/* ================================	*/
+
+USE FestiBase
+GO
+
+
+/*
+	De volgende tests gelden voor alle add_or_update procedures. Deze
+	tests triggeren namelijk een error bij een poging tot een update.
+
+	De eerste test triggert een error die aangeeft dat er geen NULL waarden 
+	aan de surrogaat sleutel variable meegegeven kan worden als er een update
+	uitgevoerd moet worden.
+
+	De tweede test triggert een error die aangeeft dat de meegeven surrogaat sleutel
+	niet bestaat. Dat moet natuurlijk wel het geval zijn om een update uit te voeren.
+*/
+
+BEGIN TRAN
+EXEC sp_add_or_update_tent NULL, 1, 'Test Tent', 1200, 2000, 500, 1000, 1400, 3300, 
+	'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000, 0
+ROLLBACK TRAN
+GO
+
+BEGIN TRAN
+EXEC sp_add_or_update_tent 300, 1, 'Test Tent', 1200, 2000, 500, 1000, 1400, 3300, 
+	'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000, 0
+ROLLBACK TRAN
+GO
+
+
+/* ================================	*/
+/*				TENT				*/
+/* ================================	*/
+
+/*
+	De volgende tests triggeren de check constraints die aan de tabel TENT zijn toegevoegd.
+*/
+
+-- Correcte INSERT
+BEGIN TRAN
+EXEC sp_add_or_update_tent NULL, 1, 'Test Tent', 1200, 2000, 500, 1000, 1400, 3300, 
+	'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000, 1
+ROLLBACK TRAN
+GO
+
+-- Correcte UPDATE
+BEGIN TRAN
+EXEC sp_add_or_update_tent 1, 1, 'Test Tent', 1200, 2000, 500, 1000, 1400, 3300, 
+	'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000, 0
+ROLLBACK TRAN
+GO
+
+-- Foute INSERT (Constructiebreedte moet groter of gelijk zijn aan de breedte)
+BEGIN TRAN
+EXEC sp_add_or_update_tent NULL, 1, 'Test Tent', 4200, 2000, 500, 1000, 1400, 3300, 
+	'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000, 1
+ROLLBACK TRAN
+GO
+
+-- Foute INSERT (Constructielengte moet groter of gelijk zijn aan de lengte)
+BEGIN TRAN
+EXEC sp_add_or_update_tent NULL, 1, 'Test Tent', 1200, 4000, 500, 1000, 1400, 3300, 
+	'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000, 1
+ROLLBACK TRAN
+GO
+
+-- Foute INSERT (hoogte moet groter of gelijk zijn aan de zijhoogte)
+BEGIN TRAN
+EXEC sp_add_or_update_tent NULL, 1, 'Test Tent', 1200, 1200, 5000, 1000, 1400, 3300, 
+	'Grote Tent', 'Blauw-wit', 'Systeemvloer', 12000, 1
+ROLLBACK TRAN
+GO
+
+
+/* ================================	*/
+/*				PODIUM				*/
+/* ================================	*/
+
+/*
+	De volgende tests triggeren de check constraints die aan de tabel PODIUM zijn toegevoegd.
+*/
+
+-- Correcte INSERT
+BEGIN TRAN
+EXEC sp_add_or_update_podium NULL, 1, 2, 'TentDestructo', 2000,2000,500, 
+	50, 5000, 50, 1900, 1700, 400, 'Leuke podium voor een leuke festival', 1    
+ROLLBACK TRAN
+GO
+
+--Correcte UDPATE
+BEGIN TRAN
+EXEC sp_add_or_update_podium 1, 1, 2, 'TentDestructo', 2000,2000,500, 
+	50, 5000, 50, 1900, 1700, 400, 'Leuke podium voor een leuke festival', 0 
+ROLLBACK TRAN
+GO
+
+-- Foute INSERT (Constructiebreedte moet groter zijn dan de vrije overspanning breedte)
+BEGIN TRAN
+EXEC sp_add_or_update_podium NULL, 1, 2, 'TentDestructo', 200,2000,500, 
+	50, 5000, 50, 1900, 1700, 400, 'Leuke podium voor een leuke festival', 1    
+ROLLBACK TRAN
+GO
+
+-- Foute INSERT (Constructielengte moet groter zijn dan de vrije overspanning lengte)
+BEGIN TRAN
+EXEC sp_add_or_update_podium NULL, 1, 2, 'TentDestructo', 2000,200,500, 
+	50, 5000, 50, 1900, 1700, 400, 'Leuke podium voor een leuke festival', 1    
+ROLLBACK TRAN
+GO
+
+-- Foute INSERT (Podiumhoogte moet groter zijn dan de vrije overspanning hoogte)
+BEGIN TRAN
+EXEC sp_add_or_update_podium NULL, 1, 2, 'TentDestructo', 2000,2000,10, 
+	50, 5000, 50, 1900, 1700, 400, 'Leuke podium voor een leuke festival', 1    
+ROLLBACK TRAN
+GO
+
+
+/* ================================	*/
+/*			PERFORMANCE				*/
+/* ================================	*/
+
+
+/*
+	De constraints op performance checken het volgende:
+	-	Of de artiest al een optreden
+	-	Of er al een artiest op dat moment op hetzelfde podium speelt
+	-	De starttijd van het optreden mag niet bekend zijn als het podium ook niet bekend is
+	-	Of het optreden past in dat podium schema (na een update)
+*/
+
+/* 
+	These tests should work
+*/
+
+-- INSERT
+BEGIN TRAN
+EXEC sp_add_or_update_performance NULL, 8, 4, 2, '15:00:00', 90, 30, 1 
+ROLLBACK TRAN
+GO
+
+BEGIN TRAN
+EXEC sp_add_or_update_performance NULL, 8, NULL, 2, NULL, 90, NULL, 1 
+ROLLBACK TRAN
+GO
+
+BEGIN TRAN
+EXEC sp_add_or_update_performance NULL, 8, NULL, 2, NULL, 90, 30, 1 
+ROLLBACK TRAN
+GO
+
+-- UPDATE
+BEGIN TRAN
+EXEC sp_add_or_update_performance 10, 8, 4, 2, '15:00:00', 90, 30, 0 
+ROLLBACK TRAN
+GO
+
+BEGIN TRAN
+EXEC sp_add_or_update_performance 10, 8, NULL, 2, NULL, 90, 30, 0
+ROLLBACK TRAN
+GO
+
+
+/*
+	These tests should NOT work
+*/
+
+-- INSERT (An artist is already playing during that time)
+BEGIN TRAN
+EXEC sp_add_or_update_performance NULL, 8, 4, 2, '20:00:00', 90, 30, 1 
+ROLLBACK TRAN
+GO
+
+-- INSERT (This artist is already going to perform during that time)
+BEGIN TRAN
+EXEC sp_add_or_update_performance NULL, 9, 4, 2, '21:00:00', 90, 30, 1 
+ROLLBACK TRAN
+GO
+
+-- INSERT (The start_time cannot be known if the schedule is NULL)
+BEGIN TRAN
+EXEC sp_add_or_update_performance NULL, 8, NULL, 2, '15:00:00', 90, 30, 1 
+ROLLBACK TRAN
+GO
+
+-- INSERT (This performance does not fit the schedule)
+BEGIN TRAN
+EXEC sp_add_or_update_performance NULL, 8, 4, 2, '13:59:00', 90, 30, 1 
+ROLLBACK TRAN
+GO
+
+-- INSERT (The artist has the wrong genre for this podium)
+BEGIN TRAN
+EXEC sp_add_or_update_performance NULL, 2, 1, 2, '14:00:00', 30, 5, 1
+ROLLBACK TRAN
+GO
+
+-- UPDATE (An artist is already playing during that time)
+BEGIN TRAN
+EXEC sp_add_or_update_performance 9, 8, 4, 2, '20:00:00', 90, 30, 0
+ROLLBACK TRAN
+GO
+
+-- UPDATE (This artist is already going to perform during that time)
+BEGIN TRAN
+EXEC sp_add_or_update_performance 9, 9, 4, 2, '21:00:00', 90, 30, 0 
+ROLLBACK TRAN
+GO
+
+-- UPDATE (The start_time cannot be known if the schedule is NULL)
+BEGIN TRAN
+EXEC sp_add_or_update_performance 9, 8, NULL, 2, '15:00:00', 90, 30, 0 
+ROLLBACK TRAN
+GO
+
+-- UPDATE (This performance does not fit the schedule)
+BEGIN TRAN
+EXEC sp_add_or_update_performance 9, 8, 4, 2, '13:59:00', 90, 30, 0
+ROLLBACK TRAN
+GO
+
+-- UPDATE (The artist has the wrong genre for this podium)
+BEGIN TRAN
+EXEC sp_add_or_update_performance 2, 2, 1, 2, '14:00:00', 30, 5, 1
+ROLLBACK TRAN
+GO
+
+
+/* ================================	*/
+/*			PODIUM_SCHEDULE			*/
+/* ================================	*/
+
+/*
+	De volgende constraints checken het volgende:
+	- Of de startdatum van een podium schema binnen de festivals datums vallen
+	- Of de einddatum van een podium schema binnen de festivals datums vallen
+*/
+
+
+-- Correcte INSERT
+BEGIN TRAN
+EXEC sp_add_or_update_podium_schedule NULL, 1, '2017-04-15', '14:00:00', '2017-04-16', '02:00:00', 30, 1
+ROLLBACK TRAN
+GO
+
+-- Correcte UPDATE
+BEGIN TRAN
+EXEC sp_add_or_update_podium_schedule 1, 1, '2017-04-15', '14:00:00', '2017-04-16', '02:00:00', 30, 0
+ROLLBACK TRAN
+GO
+
+-- Test INSERT (end date not valid)
+BEGIN TRAN
+EXEC sp_add_or_update_podium_schedule NULL, 1, '2017-04-16', '14:00:00', '2017-04-17', '02:00:00', 30, 1
+ROLLBACK TRAN
+GO
+
+-- Test INSERT (start date not valid)
+BEGIN TRAN
+EXEC sp_add_or_update_podium_schedule NULL, 1, '2017-04-13', '14:00:00', '2017-04-14', '02:00:00', 30, 1
+ROLLBACK TRAN
+GO
