228e93a1ffc6e8db71f3bed9a71caec1e83a2693
verkerk20 <robertverkerk@hetnet.nl>
2018-01-16 12:44:12 +0100

    removed unnecessary tests

--- Database/Stored Procedures/Add_or_Update/add_or_update_podium.sql
+++ Database/Stored Procedures/Add_or_Update/add_or_update_podium.sql
@@ -209,15 +209,3 @@ BEGIN TRAN
 EXEC sp_add_or_update_podium 1, 1, 2, 'Tent 1', 4000, 2000, 200, NULL, 999, 12, 1000, 1000, NULL, 'Bomen', 0
 ROLLBACK TRAN
 GO
-
-
--- Reviewed door Yuri
-BEGIN TRAN
-EXEC sp_add_or_update_podium 1, 2, 3, 'Tent 1', 4000, 2000, 200, NULL, 999, 12, 1000, 1000, NULL, 'Bomen', 1
-ROLLBACK TRAN
-GO
-
-BEGIN TRAN
-EXEC sp_add_or_update_podium 1, 2, 3, 'Tent 1', 4000, 2000, 200, NULL, 999, 12, 1000, 1000, NULL, 'Bomen', 0
-ROLLBACK TRAN
-GO
